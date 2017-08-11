b31f063684f796868aff9b1bf93c7df2abaf90cb
Matt Ellen <matt@matthewellen.co.uk>
2017-08-11 18:37:55 +0100

    extension interacting with web pages

--- highlighter.js
+++ highlighter.js
@@ -1,3 +1,25 @@
 (function()
  {
+
+     var textboxes = []
+     var t1 = document.body.querySelectorAll('[contenteditable="true"]')
+     var t2 = document.body.querySelectorAll('[type="text"]')
+     var t3 = document.getElementsByTagName('textarea')
+
+     t1.forEach(i => textboxes.push(i))
+     t2.forEach(i => textboxes.push(i))
+     for(let i = 0; i < t3.length; i++) { textboxes.push(t3[i]) }
+     
+     textboxes.forEach(tb => tb.addEventListener('keyup', function(e)
+						 {
+						     var text = this.value
+
+						     if(text === undefined)
+						     {
+							 text = this.innerText
+						     }
+
+						     console.log(text)
+						 }))
+		      
  })();
