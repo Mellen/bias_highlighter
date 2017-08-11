86027e096b2aaa9840b398c49978d7a7d20cac07
Matt Ellen <matt@matthewellen.co.uk>
2017-08-11 21:46:33 +0100

    put up a tool tip when bad words are detected

--- highlighter.js
+++ highlighter.js
@@ -1,17 +1,19 @@
 (function()
  {
-
-     var textboxes = []
-     var t1 = document.body.querySelectorAll('[contenteditable="true"]')
-     var t2 = document.body.querySelectorAll('[type="text"]')
-     var t3 = document.getElementsByTagName('textarea')
-
-     t1.forEach(i => textboxes.push(i))
-     t2.forEach(i => textboxes.push(i))
-     for(let i = 0; i < t3.length; i++) { textboxes.push(t3[i]) }
+     var textboxes = document.body.querySelectorAll('[contenteditable="true"], [type="text"], textarea')
      
      textboxes.forEach(tb => tb.addEventListener('keyup', function(e)
 						 {
+						     var tooltip = document.getElementById(this.id+'word_tip')
+
+						     if(tooltip === null)
+						     {
+							 tooltip = document.createElement('p')
+							 tooltip.id = this.id+'word_tip'
+							 tooltip.style = 'display: none;'
+							 document.body.appendChild(tooltip)
+						     }
+						     
 						     var text = this.value
 
 						     if(text === undefined)
@@ -19,7 +21,32 @@
 							 text = this.innerText
 						     }
 
-						     console.log(text)
+						     var bad_words = ['guys', 'AWFUL', 'happy christmas']
+
+						     var message = ''
+						     
+						     bad_words.forEach(function(word)
+								       {
+									   var r = new RegExp('(^|\\W)('+word+')(\\W|$)', 'gim')
+									   
+									   if(text.match(r) != null)
+									   {
+									       message += '"' + word + '" '
+									   }
+								       });
+
+						     if(message !== '')
+						     {
+							 tooltip.innerHTML = message.trim()
+							 var br = this.getBoundingClientRect()
+							 tooltip.style = 'display:block;position:absolute;background-color:black;color:white;'
+							 tooltip.style.left = br.left + 'px'
+							 tooltip.style.top = (br.top - tooltip.clientHeight) + 'px'
+						     }
+						     else
+						     {
+							 tooltip.style = 'display: none;';
+						     }
 						 }))
 		      
  })();
